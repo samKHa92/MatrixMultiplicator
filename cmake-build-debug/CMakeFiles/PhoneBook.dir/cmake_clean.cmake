d08b6ec7ccb3c62643b261260cc3ca295c00dca8
samKHa92 <samkha@gfmstudio.com>
2021-10-26 15:49:50 +0400

    mm

---
+++ cmake-build-debug/CMakeFiles/PhoneBook.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PhoneBook.dir/main.cpp.o"
+  "PhoneBook"
+  "PhoneBook.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PhoneBook.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
